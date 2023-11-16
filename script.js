e074e3097e766bb998bc87efcc0aa8e7902d1169
Harish hari <hari991996@gmail.com>
2023-11-16 15:23:25 +0530

    add title-abstract-img-link and styles-js

---
+++ script.js
@@ -0,0 +1,59 @@
+const apiKey = "P0i9UrESQqtp3L8U4UoSeG8iezdOGqab"; //  API key
+const apiUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${apiKey}`;
+const newsContainer = document.querySelector("#newsList");
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("output");
+    // received data
+    // console.log(data.results[0]);
+    data.results.forEach((result) => {
+      let articleDiv = document.createElement("div");
+      articleDiv.classList.add("article");
+      let titleH5 = document.createElement("h2");
+      titleH5.textContent = result.title;
+      articleDiv.appendChild(titleH5);
+      let abstractP = document.createElement("p");
+      abstractP.classList.add("news--description");
+      abstractP.textContent = result.abstract;
+      articleDiv.appendChild(abstractP);
+      if (
+        result.media?.length &&
+        result.media[0]["media-metadata"].length > 2
+      ) {
+        let imageImg = document.createElement("img");
+        imageImg.src = result.media[0]["media-metadata"][2].url;
+        imageImg.classList.add("news--thumb--2img");
+        articleDiv.appendChild(imageImg);
+      }
+      let newslink = document.createElement("a");
+      newslink.href = result.url;
+      newslink.target = "_blank";
+      newslink.classList.add("news--link");
+      newslink.innerHTML = "Read More";
+      articleDiv.appendChild(newslink);
+      newsContainer.appendChild(articleDiv);
+    });
+  })
+  .catch((error) => {
+    // Handle errors
+    console.log("fucked");
+    console.error("Error fetching data:", error);
+  });
+
+// const newsContainer = document.querySelector("#newsList");
+// const newsDiv = document.createElement("div");
+// newsDiv.classList.add("news--div");
+// const heading = document.createElement("h2");
+// heading.textContent = "hello world";
+// newsContainer.appendChild(newsDiv);
+// newsDiv.appendChild(heading);
+
+// data.results[0];
+// const newsDiv = document.createElement("div");
+// newsDiv.classList.add("news--div");
+// const heading = document.createElement("h2");
+// heading.textContent = data.results[0].title;
+// newsContainer.appendChild(newsDiv);
+// newsDiv.appendChild(heading);
+// // console.log(heading);
